e6f4b433f0e8ccba9a31ce7a273caa5b2c4166e9
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-09-27 23:31:17 +0900

    Title: 빙산의 일각, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240927/빙산의 일각/the-tip-of-the-iceberg.cpp
+++ 240927/빙산의 일각/the-tip-of-the-iceberg.cpp
@@ -1,49 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 int n;
 const int MaxN = 100005;
 int arr[MaxN];
+
 int main() {
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(0);
-    std::cout.tie(0);
-    std::cin >> n;
-    int ans = 1;
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    cin >> n;
+    int ans = 0;
     int left = 0;
-    int right = 10000000000;
-    for(int i = 0; i < n; i++)
-        std::cin >> arr[i];
-    while(left <= right)
-    {
+    int right = 1000000000;
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    
+    while (left + 1 < right) {
         int mid = (left + right) / 2;
         int cnt = 0;
         bool cur = false;
-        for(int i = 0; i < n; i++)
+        for (int i = 0; i < n; i++) 
         {
-            if(arr[i] > mid && !cur)
+            if (arr[i] > mid && !cur) 
             {
                 cnt++;
                 cur = true;
-            }
-            else if(arr[i] <= mid)
+            } 
+            else if (arr[i] <= mid) 
             {
                 cur = false;
             }
         }
-        if(cnt == ans)
+
+        if (cnt >= ans) 
         {
-            right = mid-1;
-        }
-        else if(cnt > ans)
-        {
-            right = mid;
             ans = cnt;
-        }
-        else if(cnt < ans)
-        {
-            left = mid+1;
+            right = mid - 1;
+        } else {
+            left = mid + 1;
         }
     }
-    std::cout << ans;
+
+    cout << ans;
     return 0;
 }
