6d75478af53873fac6b07ff9505b37f95c3c8be6
ckdlscjs <41976800+ckdlscjs@users.noreply.github.com>
2024-09-29 11:16:31 +0900

    Title: 빙산의 일각, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240929/빙산의 일각/the-tip-of-the-iceberg.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n;
+const int MaxN = 100005;
+int arr[MaxN];
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(0);
+    std::cout.tie(0);
+    std::cin >> n;
+    int ans = 0;
+    int left = 0;
+    int right = 0;
+    for(int i = 0; i < n; i++)
+    {
+        std::cin >> arr[i];
+        right = std::max(right, arr[i]);
+    }
+        
+    while(left <= right)
+    {
+        int mid = (left + right) / 2;
+        int cnt = 0;
+        bool cur = false;
+        for(int i = 0; i < n; i++)
+        {
+            if(arr[i] > mid && !cur)
+            {
+                cnt++;
+                cur = true;
+            }
+            else if(arr[i] <= mid)
+            {
+                cur = false;
+            }
+        }
+        if(cnt >= ans)
+        {
+            right = mid - 1;
+            ans = cnt;
+        }
+        else if(cnt < ans)
+        {
+            left = mid+1;
+        }
+    }
+    std::cout << ans;
+    return 0;
+}
